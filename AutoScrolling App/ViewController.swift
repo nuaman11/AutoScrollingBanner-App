618cde2bfd7b0890a0806f78917c8061d3e194db
nuaman <nuaman.s@axeltechnologies.com>
2024-11-19 12:57:26 +0530

    Initial Commit

---
+++ AutoScrolling App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoScrolling App
+//
+//  Created by Muhammed Nuaman Sadiq on 19/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
